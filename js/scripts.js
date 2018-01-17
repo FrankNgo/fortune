86f015da9da121c80353f27e850fdf9620bb6399
frank ngo and roger yee <roger@>
2018-01-17 13:59:29 -0800

    makes fortune-teller work

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,45 @@
 $(document).ready(function() {
-    event.preventDefault();
+  $("form#fortuneForm").submit(function(event){
+  var result = 0;
 
+  event.preventDefault();
+
+  $("input:checkbox[name=luckVal]:checked").each(function(){
+    var luckCount = $(this).val();
+
+    if(luckCount === "beginnerLuck"){
+      result += 1;
+    } else if (luckCount === "foundPenny"){
+      result += 1;
+    } else if (luckCount === "underLadder"){
+      result -= 1;
+    } else if (luckCount === "blackCat"){
+      result -= 1;
+    } else if (luckCount === "brokeMirror"){
+      result -= 1;
+    } else if (luckCount === "saw666"){
+      result -= 1;
+    } else if (luckCount === "knockWood"){
+      result += 1;
+    } else if (luckCount === "umbrellaInside"){
+      result -= 1;
+    } else if (luckCount === "friday13"){
+      result -= 1;
+    }
+
+    });
+
+    if (result > 0) {
+      $("#goodLuck").show();
+      $(".container").hide();
+
+    } else if (result < 0) {
+      $("#badLuck").show();
+      $(".container").hide();
+      
+    } else if (result === 0) {
+      $("#neutralLuck").show()
+      $(".container").hide();
+    }
+  });
 });
